a81e70969a10f8d24a206e711c290a2799ab532c
Rizky Reranza <rranzabussines@gmail.com>
2026-08-11 21:52:38 +0900

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@ import { imageHosts } from './image-hosts.config.mjs';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/king-decisionV2',
+  images: {
+    unoptimized: true,
+    remotePatterns: imageHosts,
+    minimumCacheTTL: 60,
+    qualities: [75, 85, 100],
+  },
+
   productionBrowserSourceMaps: true,
   distDir: process.env.DIST_DIR || '.next',
 
@@ -12,11 +21,5 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-
-  images: {
-    remotePatterns: imageHosts,
-    minimumCacheTTL: 60,
-    qualities: [75, 85, 100],
-  }
 };
 export default nextConfig;
